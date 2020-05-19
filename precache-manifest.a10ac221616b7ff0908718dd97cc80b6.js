3cd04e248d8375bc23f91b7a4cc8e2e69cb7bca8
Aman gupta <aman06016@gmail.com>
2020-05-19 23:43:23 +0530

    Updates

---
+++ precache-manifest.a10ac221616b7ff0908718dd97cc80b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4fa15b0d7c115751d44a",
+    "url": "/monsters-rolodex/static/css/main.f66e5723.chunk.css"
+  },
+  {
+    "revision": "4fa15b0d7c115751d44a",
+    "url": "/monsters-rolodex/static/js/main.e2512bde.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "73c74a9bb63118e4d3d0",
+    "url": "/monsters-rolodex/static/js/2.e3a2b14b.chunk.js"
+  },
+  {
+    "revision": "d93957375afc526ddd485612459bd72a",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
